19b222e8365a4a4181dbc3c0306fa737b7436049
nhegmus@gmail.com <nhegm>
2023-07-24 16:10:24 +0800

    Test ready

---
+++ src/test/java/ru/netology/PostmanEchoTestV1.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTestV1 {
+    @Test
+    void shouldSendSomeData() {
+      given()
+              .baseUri("https://postman-echo.com")
+              .body("some data")
+
+              .when()
+              .post("/post")
+
+              .then()
+              .statusCode(200)
+              .body("data", equalTo("some data"))
+      ;
+    }
+}
